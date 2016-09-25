779826e196f3bf6cb221a6332dd6cc3c8ff179ce
Géraud CONTINSOUZAS <gcs@cpan.org>
2016-09-26 08:45:44 +1100

    Set the proper signal to run the script.

--- script/service.sh
+++ script/service.sh
@@ -22,21 +22,21 @@ then
 fi
 
 MY_APP="altpan"
-MY_PORT=5002
+MY_HOST_PORT="127.0.0.1:5002"
 
 SERVER_CMD=`which start_server`
-SERVER_OPTS="--dir=${BASEDIR} --signal-on-hup=HUP --pid-file=${RUNDIR}/${MY_APP}.pid --status-file=${RUNDIR}/${MY_APP}.status --log-file=${LOGDIR}/${MY_APP}.log"
+SERVER_OPTS="--dir=${BASEDIR} --signal-on-hup=QUIT --pid-file=${RUNDIR}/${MY_APP}.pid --status-file=${RUNDIR}/${MY_APP}.status --log-file=${LOGDIR}/${MY_APP}.log"
 
 
 ss_start () {
     ${SERVER_CMD} ${SERVER_OPTS} \
-        --port=0:${MY_PORT} --daemonize --enable-auto-restart=1 -- \
-        starman app.psgi
+        --port=${MY_HOST_PORT} --daemonize --enable-auto-restart=1 -- \
+        starman --listen ${MY_HOST_PORT} app.psgi
 }
 
 ss_restart () {
     ${SERVER_CMD} ${SERVER_OPTS} \
-        --port=0:${MY_PORT} --daemonize --enable-auto-restart=1 --restart
+        --port=${MY_HOST_PORT} --daemonize --enable-auto-restart=1 --restart
 }
 
 ss_stop () {
